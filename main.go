9286c6b4a40adb36fafd71464e7c1905c397bdad
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2023-02-19 22:56:23 +0400

    Rename handler for root path

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	// this can determine how a browser would render response body
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
@@ -17,7 +17,7 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/contact", contactHandler)
 	_ = http.ListenAndServe(":3000", nil)
 }
