bdd29c8f3a8338df102a15124f8baa800dc19318
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2023-02-19 22:56:04 +0400

    Add contact handler
    Yeah, I haven't changed the mail

--- main.go
+++ main.go
@@ -11,7 +11,13 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
+}
+
 func main() {
 	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/contact", contactHandler)
 	_ = http.ListenAndServe(":3000", nil)
 }
