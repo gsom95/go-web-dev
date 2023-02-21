65eaea65f95ba703e34061b5c98036099609084a
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2023-02-21 11:14:01 +0400

    Add an example with unescaped html input for template

--- cmd/template_exp/main.go
+++ cmd/template_exp/main.go
@@ -8,6 +8,7 @@ import (
 
 type User struct {
 	Name string
+	Bio  string
 }
 
 func main() {
@@ -16,7 +17,10 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	user := User{"John Kek"}
+	user := User{
+		Name: "John Kek",
+		Bio:  `<script>alert("Haha, you have been h4x0r3d!");</script>`,
+	}
 	err = t.Execute(os.Stdout, user)
 	if err != nil {
 		log.Fatalln(err)
