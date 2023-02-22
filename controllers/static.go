207180d9928e4a2fae5ee8860f64bf9e01c636f7
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2023-02-22 18:12:49 +0400

    Change static controller to return a closure.
    With this approach we can now use chi.Get-like methods again. Because we return a HandlerFunc.

--- controllers/static.go
+++ controllers/static.go
@@ -6,12 +6,9 @@ import (
 	"github.com/gsom95/go-web-dev/view"
 )
 
-// Static controller serves static web pages from a template.
-type Static struct {
-	Template view.Template
-}
-
-// ServeHttp implements http.Handler interface.
-func (s Static) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	s.Template.Execute(w, nil)
+// StaticHandler returns a HandlerFunc for serving static web pages.
+func StaticHandler(tpl view.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, nil)
+	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -12,57 +11,31 @@ import (
 	"github.com/gsom95/go-web-dev/view"
 )
 
-func executeTemplate(w http.ResponseWriter, filepath string) {
-	tpl, err := view.Parse(filepath)
-	if err != nil {
-		log.Printf("processing template: %v", err)
-		http.Error(w, "There was an error processing the template.", http.StatusInternalServerError)
-		return
-	}
-	tpl.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "home.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "faq.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "contact.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func getGallery(w http.ResponseWriter, r *http.Request) {
-	galleryID := chi.URLParam(r, "galleryID")
-	fmt.Fprintf(w, "Got id=%s", galleryID)
-}
-
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger, middleware.Recoverer)
-
-	homeTpl, err := view.Parse(filepath.Join("templates", "home.gohtml"))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	r.Method(http.MethodGet, "/", controllers.Static{Template: homeTpl})
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
-	r.Get("/panic", func(w http.ResponseWriter, r *http.Request) {
-		panic("at the disco!")
-	})
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 	})
-	r.Route("/galleries", func(r chi.Router) {
-		r.Get("/{galleryID}", getGallery)
-	})
 
-	fmt.Println("Starting the server on :3000...")
+	tpl, err := view.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = view.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = view.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	r.Get("/faq", controllers.StaticHandler(tpl))
+
+	log.Println("Starting the server on :3000...")
 	_ = http.ListenAndServe(":3000", r)
 }
