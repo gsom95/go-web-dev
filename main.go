0a69e982ef7a1155c2b543603b15fbf10a38a257
gsom95 <i.sapozhnikov@propellerads.net>
2023-02-23 17:30:10 +0400

    Work with separate templates for header and footer

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	})
 
 	r.Get("/", controllers.StaticHandler(
-		view.Must(view.ParseFS(templates.FS, "home.gohtml")),
+		view.Must(view.ParseFS(templates.FS, "home.gohtml", "layout-parts.gohtml")),
 	))
 	r.Get("/contact", controllers.StaticHandler(
 		view.Must(view.ParseFS(templates.FS, "contact.gohtml")),
