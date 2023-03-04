e603f183c79126959e13d174d960da75f1e7e0fe
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2023-03-04 12:25:57 +0400

    Add pre-filling form with query params

--- controllers/users.go
+++ controllers/users.go
@@ -14,7 +14,11 @@ type Users struct {
 
 // New shows signup page.
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
-	u.Templates.New.Execute(w, nil)
+	var data struct {
+		Email string
+	}
+	data.Email = r.FormValue("email")
+	u.Templates.New.Execute(w, data)
 }
 
 // Create handles creation of a user
