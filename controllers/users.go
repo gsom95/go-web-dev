a4d3e11c6cd60bacadcddb079b02b7e27094f60a
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2023-03-05 20:47:54 +0400

    Revert "Solve Ex1 - Experiment With Form Fields"
    This reverts commit 6521cb87c7ddb710d8bc486d24f93ea3419717c6.

--- controllers/users.go
+++ controllers/users.go
@@ -26,7 +26,4 @@ func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 	// for now r.FormValue would be enough because it calls r.ParseForm()
 	fmt.Fprintf(w, "<p>Email: %s</p>\n", r.FormValue("email"))
 	fmt.Fprintf(w, "<p>Password: %s</p>\n", r.FormValue("password"))
-	fmt.Fprintf(w, "<p>Bio: %s</p>\n", r.FormValue("bio"))
-	fmt.Fprintf(w, "<p>Consent: %s</p>\n", r.FormValue("consent"))
-	fmt.Fprintf(w, "<p>Favourite language: %s</p>\n", r.FormValue("fav_language"))
 }
