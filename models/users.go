36b309f0a4891abfea01431a760f7640ac669f1b
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2023-03-11 22:20:13 +0400

    Add UserService
    "Using a UserService type with a *sql.DB field can also help encourage developers to write SQL-related code in the correct location - inside our models package as a method on the UserService type - rather than placing SQL queries anywhere and everywhere in our code that the *sql.DB object happens to be available. "

--- models/users.go
+++ models/users.go
@@ -1,7 +1,14 @@
 package models
 
+import "database/sql"
+
 type User struct {
 	ID           int
 	Email        string
 	PasswordHash string
+}
+
+// UserService will interact with database.
+type UserService struct {
+	DB *sql.DB
 }
