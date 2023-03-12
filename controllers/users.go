dd405e58ecda1619b260f6ce07deba378b5eb6db
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2023-03-12 15:43:19 +0400

    Implement create method for controller
    We have now successfully learned how to connect all of the pieces of our application:
    
    1. We create a controller type like controllers.Users.
    2. Next we add fields to this type with views that we need to render pages.
    3. We also add fields to the type for services we need access to from our models package.
    4. When setting up our application, we make sure these fields are all assigned correctly.

--- controllers/users.go
+++ controllers/users.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gsom95/go-web-dev/models"
@@ -26,7 +27,13 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 
 // Create handles creation of a user
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	// for now r.FormValue would be enough because it calls r.ParseForm()
-	fmt.Fprintf(w, "<p>Email: %s</p>\n", r.FormValue("email"))
-	fmt.Fprintf(w, "<p>Password: %s</p>\n", r.FormValue("password"))
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	user, err := u.Service.Create(email, password)
+	if err != nil {
+		log.Println("error creaing new user:", err.Error())
+		http.Error(w, "Something went wrong while creating a user", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "User created: %+v", user)
 }
