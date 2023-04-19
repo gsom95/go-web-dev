91c7edb78fe1b751846d6317e83fb72a55b7b1ce
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2023-04-19 22:57:52 +0400

    Create SessionService and Create method stub

--- models/session.go
+++ models/session.go
@@ -1,8 +1,34 @@
 package models
 
+import "database/sql"
+
 // Session is a one-to-one mapping of sessions table.
 type Session struct {
-	ID        int
-	UserID    int
+	ID     int
+	UserID int
+	// Token is only set when creating a new session. When looking up a session
+	// this will be left empty, as we only store the hash of a session token
+	// in our database and we cannot reverse it into a raw token.
+	Token     string
 	TokenHash string
+}
+
+// SessionService is a service for storing and fetching session info.
+type SessionService struct {
+	DB *sql.DB
+}
+
+// Create will create a new session for the user provided. The session token
+// will be returned as the Token field on the Session type, but only the hashed
+// session token is stored in the database.
+func (ss *SessionService) Create(userID int) (*Session, error) {
+	// TODO: Create the session token
+	// TODO: Implement SessionService.Create
+	return nil, nil
+}
+
+// User queries a user via raw token using the SessionService.
+func (ss *SessionService) User(token string) (*User, error) {
+	// TODO: Implement SessionService.User
+	return nil, nil
 }
