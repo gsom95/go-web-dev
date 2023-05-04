6b8070ef5ad9e760397187ffad340d9a2d973a8a
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2023-05-04 14:41:01 +0400

    Add pkg for storing and retrieving user data from a context

---
+++ context/users.go
@@ -0,0 +1,31 @@
+package context
+
+import (
+	"context"
+
+	"github.com/gsom95/go-web-dev/models"
+)
+
+type key string
+
+const (
+	userKey key = "user"
+)
+
+// WithUser returns context with user data.
+func WithUser(ctx context.Context, user *models.User) context.Context {
+	return context.WithValue(ctx, userKey, user)
+}
+
+// User retrieves user data from a context.
+func User(ctx context.Context) *models.User {
+	val := ctx.Value(userKey)
+	user, ok := val.(*models.User)
+	if !ok {
+		// The most likely case is that nothing was ever stored in the context,
+		// so it doesn't have a type of *models.User. It is also possible that
+		// other code in this package wrote an invalid value using the user key.
+		return nil
+	}
+	return user
+}
