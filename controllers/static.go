835ffee660c229179709c964fd06a98469c4ec2a
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2025-07-24 07:34:55 +0400

    Section 7 - Enhancing our Views
    Refactor FAQ page to populate the content from the parameters set in code

--- controllers/static.go
+++ controllers/static.go
@@ -11,3 +11,26 @@ func StaticHandler(tpl views.Template) http.HandlerFunc {
 		tpl.Execute(w, nil)
 	}
 }
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   string
+	}{
+		{
+			Question: "Is there a free version?",
+			Answer:   "Yes! We offer a free trial for 30 days on any paid plans.",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer:   "We have support staff answering emails 24/7, though response times may be a bit slower on weekends.",
+		},
+		{
+			Question: "How do I contact support?",
+			Answer:   `Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>`,
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
+	}
+}
